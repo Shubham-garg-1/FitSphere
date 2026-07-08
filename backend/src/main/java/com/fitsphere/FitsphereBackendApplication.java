103fe9e8e9f426c244284ec55d6abb1b923507f5
Shubham-garg-1 <shubh.gargofficial@gmail.com>
2026-07-09 00:48:55 +0530

    Initial Project Setup

---
+++ backend/src/main/java/com/fitsphere/FitsphereBackendApplication.java
@@ -0,0 +1,13 @@
+package com.fitsphere;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitsphereBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitsphereBackendApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/fitsphere/FitsphereBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fitsphere;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitsphereBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
